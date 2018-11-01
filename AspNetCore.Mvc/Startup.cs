ea8775aa2c64d5f0110e82a5fcb12f8d77dc9910
xutian <xutian0521@qq.com>
2018-11-01 15:45:31 +0800

    add ConnectionStrings

--- AspNetCore.Mvc/Startup.cs
+++ AspNetCore.Mvc/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
