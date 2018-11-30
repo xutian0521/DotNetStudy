252a41bb6a425b4bd5bc30272ec9290f8609be95
xutian <xutian0521@qq.com>
2018-11-30 14:57:57 +0800

    add TestConcurrency RedistInsertTest

---
+++ RedistInsertTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace RedistInsertTest
+{
+    class Program
+    {
+        private static readonly string Coonstr = "192.168.3.16,password=Ji0qneD47Y2Hz7UA";
+        private static object _locker = new Object();
+        private static ConnectionMultiplexer _instance = _instance = StackExchange.Redis.ConnectionMultiplexer.Connect(Coonstr);
+        static Random random = new Random();
+        static async Task Main()
+        {
+            CheckDiscountRate("1.5");
+            while (true)
+            {
+                string sj = DateTime.Now.ToString("yyMMddHHmmssfff") + random.Next(0, 1000);
+                //string sj = DateTime.Now.Ticks.ToString();
+
+                var r = await _instance.GetDatabase(14).StringSetAsync("StackRedis:" + sj, "1");
+
+                //_instance.GetDatabase(14).ListLeftPush("StackRedis", sj);
+            }
+            //Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 检查折让率是否正确
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool CheckDiscountRate(string s)
+        {
+            bool isRight = true;
+            if (System.Text.RegularExpressions.Regex.IsMatch(s, @"^\d{1,2}(\.\d)?$") == false)
+            {
+                //只能是1位小数!
+                isRight = false;
+            }
+            if (Convert.ToDecimal(s) < 0)
+            {
+                //不能低于0!
+                isRight = false;
+            }
+            return isRight;
+        }
+    }
+}
---
+++ TestConcurrency/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace TestConcurrency
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  userid=0");
+            //    var row = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserID=1 WHERE id={id} and UserID=0");
+            //    if (row > 0)
+            //    {
+            //        await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserID) VALUES(NEWID(),{id})");
+            //    }
+
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.Run(async (context) =>
+            {
+                int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  userid=0");
+                var row = await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserID) SELECT null,{id} WHERE NOT EXISTS(SELECT 1  FROM dbo.t_hcwtest WHERE UserID={id})");
+                var row2 = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserID=1 WHERE id={id} and UserID=0");
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+        }
+        public static IDbConnection Conn()
+        {
+            return new SqlConnection("Server=36.7.138.114;UID=sa;PWD=Aa4008577006;database=ws_db;");
+        }
+    }
+}
