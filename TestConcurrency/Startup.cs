d4f998cbe3e7d5e098c556d9e765bc6f601ab95f
xutian <xutian0521@qq.com>
2019-02-18 13:42:25 +0800

    高并发test 注释说明

--- TestConcurrency/Startup.cs
+++ TestConcurrency/Startup.cs
@@ -28,23 +28,26 @@ namespace TestConcurrency
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.Run(async (context) =>
-            //{
-            //    int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  userid=0");
-            //    var row = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserID=1 WHERE id={id} and UserID=0");
+            // app.Run(async (context) =>
+            // {
+            //    int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  UserFlag=0");
+            //    // 这是flag标记 解决并发插入问题 解决insert不能用 update where 方法
+            //    var row = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserFlag=1 WHERE id={id} and UserFlag=0");
             //    if (row > 0)
             //    {
-            //        await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserID) VALUES(NEWID(),{id})");
+            //        await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserFlag) VALUES(NEWID(),{id})");
             //    }
 
             //    await context.Response.WriteAsync("Hello World!");
-            //});
+            // });
 
             app.Run(async (context) =>
             {
-                int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  userid=0");
-                var row = await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserID) SELECT null,{id} WHERE NOT EXISTS(SELECT 1  FROM dbo.t_hcwtest WHERE UserID={id})");
-                var row2 = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserID=1 WHERE id={id} and UserID=0");
+                int id = await Conn().QueryFirstOrDefaultAsync<int>("SELECT Top 1 id FROM [ws_db].[dbo].[t_hcwtest] WHERE  UserFlag=0");
+                //1. sqlserver 的 EXISTS SELECT防并发-- 只能放在少了并发
+                var row = await Conn().ExecuteAsync($"INSERT INTO dbo.t_hcwtest (GUID,UserFlag) SELECT null,{id} WHERE NOT EXISTS(SELECT 1  FROM dbo.t_hcwtest WHERE UserFlag={id})");
+                //2. update where 防并发。 关系型数据库通用的防并发方法-- 利用数据库在更新通一条记录的时候 是原子操作，更新过程：当数据库要更新某一行的时候会在当前行或表 加上排它锁, 这样其他的修改数据库的操作都无法进行。更新完成后释放排它锁
+                var row2 = await Conn().ExecuteAsync($"UPDATE [ws_db].[dbo].[t_hcwtest] SET UserFlag=1 WHERE id={id} and UserID=0");
                 await context.Response.WriteAsync("Hello World!");
             });
 
